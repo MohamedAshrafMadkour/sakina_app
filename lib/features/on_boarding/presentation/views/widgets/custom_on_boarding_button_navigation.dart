0875df94e4f4c7251fb74791b00e69ee1c3db343
Mohamed Madkour <madkourmohamed88@gmail.com>
2026-02-06 11:52:42 +0200

    handle errors

--- lib/features/on_boarding/presentation/views/widgets/custom_on_boarding_button_navigation.dart
+++ lib/features/on_boarding/presentation/views/widgets/custom_on_boarding_button_navigation.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:sakina_app/core/constants/app_colors/light_app_colors.dart';
-import 'package:sakina_app/core/constants/app_routes.dart';
 import 'package:sakina_app/core/constants/styles/app_styles.dart';
 import 'package:sakina_app/features/on_boarding/presentation/views/widgets/custom_arrow.dart';
 import 'package:sakina_app/features/on_boarding/presentation/views/widgets/custom_on_boarding_next_button.dart';
